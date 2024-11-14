a7fbebd564724ee3a0ebfa36e731e339edc6054f
yurim <yrseo1216@gmail.com>
2024-11-14 16:32:54 +0900

    [feat] 초기 개발 세팅
    필요한 패키지 설정 및 react 설정

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	overrides: [],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+	// 원하는 규칙 추가하기
+	rules: {
+		quotes: ['error', 'single'],
+		'no-duplicate-imports': 'error',
+		'no-console': ['warn', { allow: ['warn', 'error', 'info'] }],
+		'no-unused-vars': 'error',
+		'no-multiple-empty-lines': 'error',
+	},
+	settings: {
+		'import/resolver': {
+			typescript: {},
+		},
+	},
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+};
